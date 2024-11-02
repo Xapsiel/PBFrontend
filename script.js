224b09f929ffa4dc44bbd62c8d6c51f3cbe90370
Xapsiel <ilyas_useinov3301@mail.ru>
2024-11-02 15:18:06 +0300

    переписана структура кода

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
+const pixelSize = 10;
+const cooldownTime = 10000;
+let socket = null;
 
-const canvas = document.getElementById('pixelCanvas');
-const ctx = canvas.getContext('2d');
-const pixelSize = 10; // Размер пикселя
-let isDrawing = false;
-let socket = null; // WebSocket переменная
-let lastSendTime = 0; // Время последней отправки
-const cooldownTime = 10000; // Кулдаун 10 секунд
-const coordinates = document.getElementById("coordinateDisplay")
-var prevx = 0
-var prevy = 0
-// Функция для входа
+const state = {
+  isDrawing: false,
+  lastSendTime: 0,
+  prevCoords: { x: 0, y: 0 },
+};
+
+const elements = {
+  canvas: document.getElementById('pixelCanvas'),
+  ctx: document.getElementById('pixelCanvas').getContext('2d'),
+  coordinates: document.getElementById("coordinateDisplay"),
+  logoutButton: document.getElementById("exitBtn"),
+  formOpenBtn: document.querySelector("#form-open"),
+  home: document.querySelector(".home"),
+  registerButton: document.querySelector("#registerButton"),
+  loginBtn :document.querySelector("#loginBtn"),
+  loginContainer: document.querySelector(".login_form"),
+  signUpContainer: document.querySelector(".signup_form"),
+  errorDisplay: document.getElementById('error-display'),
+  errorText: document.getElementById('error-text'),
+  signupBtn:document.querySelector("#signupBtn"),
+  formContainer : document.querySelector(".form_container"),
+  toLoginBtn: document.querySelector("#toLogin"),
+
+};
+
+const auth = {
+  token: window.localStorage.getItem("jwtToken"),
+  id: window.localStorage.getItem("id"),
+};
+
+// Вход в систему
 async function signIn(login, password) {
   try {
-    const response = await axios.post("http://localhost:8080/auth/sign-in", {
-      login,
-      password,
-    });
-    if (response.data.token) {
-      const jwtToken = response.data.token;
-      const id = response.data.id;
-      window.localStorage.setItem("jwtToken", jwtToken);
+    const response = await axios.post("http://localhost:8080/auth/sign-in", { login, password });
+    const { token, id } = response.data;
+    if (token) {
+      auth.token = token;
+      auth.id = id;
+      window.localStorage.setItem("jwtToken", token);
       window.localStorage.setItem("id", id);
-      return jwtToken;
+      return token;
     } else {
       throw new Error("No token received during sign-in.");
     }
@@ -31,39 +52,31 @@ async function signIn(login, password) {
   }
 }
 
-// Функция для регистрации
+// Регистрация
 async function signUp(login, email, password, repeatPassword) {
-  window.localStorage.setItem("jwtToken", "");
-  console.log(window.localStorage.getItem("jwtToken"))
   try {
     const response = await axios.post("http://localhost:8080/auth/sign-up", {
-      login,
-      email,
-      password,
-      repeatpassword: repeatPassword,
+      login, email, password, repeatpassword: repeatPassword,
     });
     if (response.data.status === "success") {
-      await signIn(login, password); // Вход после успешной регистрации
+      await signIn(login, password);
+      elements.home.classList.remove("show");
+      elements.formOpenBtn.style.display = "none";
     }
     return response.data;
+
   } catch (error) {
     console.error("Error during SignUp:", error);
     throw error;
   }
 }
 
-// Получение времени последнего клика с сервера
+// Получение последнего времени клика
 async function getLastClickFromServer() {
   try {
-    const id = parseInt(window.localStorage.getItem("id"));
     const response = await axios.post('http://localhost:8080/api/getLastClick', {
-      id: id,
-    }, {
-      headers: {
-        "Authorization": `Bearer ${window.localStorage.getItem("jwtToken")}`,
-      },
-    });
-
+      id: parseInt(auth.id),
+    }, { headers: { "Authorization": `Bearer ${auth.token}` } });
     return response.data.lastclick;
   } catch (error) {
     console.error("Error fetching last click from server:", error);
@@ -71,242 +84,125 @@ async function getLastClickFromServer() {
   }
 }
 
-// Функция для закрашивания всех пикселей в белый цвет
+// Заполнение белым цветом
 function fillCanvasWithWhite() {
-  const cols = canvas.width / pixelSize;
-  const rows = canvas.height / pixelSize;
+  const cols = elements.canvas.width / pixelSize;
+  const rows = elements.canvas.height / pixelSize;
   for (let x = 0; x < cols; x++) {
     for (let y = 0; y < rows; y++) {
-      drawPixel(x, y, '#FFFFFF'); // Закрашиваем пиксели в белый
+      drawPixel(x, y, '#FFFFFF');
     }
   }
 }
 
-// Функция для получения пикселей
+// Получение и рисование пикселей
 async function fetchPixels() {
+  fillCanvasWithWhite();
   try {
-    fillCanvasWithWhite(); // Закрашиваем белым
-
     const response = await axios.get('http://localhost:8080/pixels/getPixels', {
       headers: {
-        "Authorization": `Bearer ${window.localStorage.getItem("jwtToken")}`,
+        "Authorization": `Bearer ${auth.token}`,
         "Content-Type": "application/json; charset=UTF-8",
       },
     });
-
-    response.data.forEach(pixel => {
-      drawPixel(pixel.x, pixel.y, pixel.color);
-    });
+    response.data.forEach(pixel => drawPixel(pixel.x, pixel.y, pixel.color));
   } catch (error) {
     console.error('Error fetching pixels:', error);
     throw error;
   }
 }
 
-// Функция отправки данных пикселя
+// Отправка пикселя
 async function sendPixelData(x, y, color) {
-  // Проверяем, был ли инициализирован WebSocket
   if (!socket || socket.readyState !== WebSocket.OPEN) {
     console.error("WebSocket не инициализирован или не подключен.");
-    return false; // Прекращаем выполнение, если WebSocket недоступен
+    return false;
   }
-  lastSendTime = await getLastClickFromServer(); // Получаем последний клик
+  state.lastSendTime = await getLastClickFromServer();
   const currentTime = Date.now();
-
-  // Проверка кулдауна
-  if (currentTime - lastSendTime < cooldownTime) {
-    console.log("Пиксель не отправлен, кулдаун ещё активен");
-    showError("Кулдаун не прошел")
+  if (currentTime - state.lastSendTime < cooldownTime) {
+    showError("Кулдаун не прошел");
     return false;
   }
 
-  // Данные пикселя
-  const pixelData = {
-    x,
-    y,
-    color,
-    lastclick: currentTime,
-    id: parseInt(window.localStorage.getItem("id")),
-  };
-
-
-  // Отправляем данные через WebSocket
+  const pixelData = { x, y, color, lastclick: currentTime, id: parseInt(auth.id) };
   socket.send(JSON.stringify(pixelData));
   return true;
 }
 
-// Функция для затемнения цвета
-function getSlightlyDarkerColor(hexColor) {
-  let r = parseInt(hexColor.slice(1, 3), 16);
-  let g = parseInt(hexColor.slice(3, 5), 16);
-  let b = parseInt(hexColor.slice(5, 7), 16);
-
-  const offset = 0; // На сколько затемнить
-  r = Math.max(0, r - offset);
-  g = Math.max(0, g - offset);
-  b = Math.max(0, b - offset);
-
-  return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
-}
-
-// Функция рисования пикселя
+// Рисование пикселя
 function drawPixel(x, y, color) {
-  ctx.fillStyle = color;
-
-  ctx.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize); // Рисуем пиксель
+  elements.ctx.fillStyle = color;
+  elements.ctx.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
 }
 
 // Инициализация WebSocket
 function initWebSocket() {
-  socket = new WebSocket(`ws://localhost:8080/webhook/ws?token=${window.localStorage.getItem("jwtToken")}`);
-
-  socket.onopen = () => {
-    console.log("Connected to WebSocket server");
-    fetchPixels(); // Получаем пиксели после подключения
-  };
-
-  socket.onmessage = (event) => {
+  socket = new WebSocket(`ws://localhost:8080/webhook/ws?token=${auth.token}`);
+  socket.onopen = fetchPixels;
+  socket.onmessage = event => {
     const data = JSON.parse(event.data);
     drawPixel(data.x, data.y, data.color);
   };
-
-  socket.onclose = (event) => {
-    console.log(`WebSocket closed: ${event.code} - ${event.reason}`);
-    if (event.code === 1001) {
-      console.error("WebSocket closed with code 1001 (going away)");
-    }
-    // Переподключение
-    setTimeout(initWebSocket, 2000);
-  };
+  socket.onclose = () => setTimeout(initWebSocket, 2000);
 }
 
 // Получение координат пикселя
 function getPixelCoordinates(event) {
-  const rect = canvas.getBoundingClientRect();
-  const x = Math.floor((event.clientX - rect.left) / pixelSize);
-  const y = Math.floor((event.clientY - rect.top) / pixelSize);
-  return { x, y };
-}
-
-// Обработка событий мыши
-canvas.addEventListener('mousedown', async (event) => {
-  if (!isAuthenticated()) {
-    showError("Вы должны быть авторизованы, чтобы рисовать пиксели.");
-    return; // Прекращаем выполнение функции, если не авторизован
-  }
-  isDrawing = true;
-  const { x, y } = getPixelCoordinates(event);
-
-  const color = document.querySelector('input[name="color"]:checked').value; // Получаем выбранный цвет
-
-  if (await sendPixelData(x, y, color) ) {
-    drawPixel(x, y, color); // Рисуем пиксель на клиенте
-  }
-});
-// Обработка событий при движении мыши
-canvas.addEventListener('mousemove', (event) => {
-  // Получаем координаты пикселя
-  const { x, y } = getPixelCoordinates(event);
-  if (x===prevx && y===prevy){
-    return
-  }
-  // Обновляем координаты на экране
-  coordinates.textContent = `x: ${x}, y: ${y}`;
-  prevx = x;
-  prevy = y;
-});
-canvas.addEventListener('mouseup', () => {
-  isDrawing = false;
-});
-
-// Функция отправки пикселей
-async function sendPixels(pixels) {
-  try {
-    const response = await axios.post('http://localhost:8080/api/print', pixels, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${window.localStorage.getItem('jwtToken')}`,
-      },
-    });
-  } catch (error) {
-    console.error('Error sending pixels:', error);
-  }
-}
-
-
-// Функция для рисования квадрата
-async function printSquare(x1, y1, x2, y2, color) {
-  const pixels = [];
-
-  for (let y = y1; y <= y2; y++) {
-    for (let x = x1; x < x2; x++) {
-      pixels.push({
-        x,
-        y,
-        color,
-      });
-    }
-  }
-  const pixelsData = {
-    id: 1,
-    data: pixels,
+  const rect = elements.canvas.getBoundingClientRect();
+  return {
+    x: Math.floor((event.clientX - rect.left) / pixelSize),
+    y: Math.floor((event.clientY - rect.top) / pixelSize),
   };
-  await sendPixels(pixelsData);
 }
 
-const formOpenBtn = document.querySelector("#form-open"),
-    home = document.querySelector(".home"),
-    formContainer = document.querySelector(".form_container"),
-    formCloseBtn = document.querySelector(".form_close"),
-    signupBtn = document.querySelector("#signupBtn"),
-    loginBtn = document.querySelector("#loginBtn"),
-    toLogin = document.querySelector("#toLogin"),
-    pwShowHide = document.querySelectorAll(".pw_hide");
+// Ошибки
+function showError(message) {
+  elements.errorText.textContent = message;
+  elements.errorDisplay.style.opacity = '1';
+  setTimeout(() => elements.errorDisplay.style.opacity = '0', 5000);
+}
 
-formOpenBtn.addEventListener("click", () => home.classList.add("show"));
-formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
-
-pwShowHide.forEach((icon) => {
-  icon.addEventListener("click", () => {
-    let getPwInput = icon.parentElement.querySelector("input");
-    if (getPwInput.type === "password") {
-      getPwInput.type = "text";
-      icon.classList.replace("uil-eye-slash", "uil-eye");
-    } else {
-      getPwInput.type = "password";
-      icon.classList.replace("uil-eye", "uil-eye-slash");
-    }
-  });
+// Обработка событий
+elements.canvas.addEventListener('mousedown', async (event) => {
+  if (!await isAuthenticated()) {
+    showError("Вы должны быть авторизованы, чтобы рисовать пиксели.");
+    return;
+  }
+  state.isDrawing = true;
+  const { x, y } = getPixelCoordinates(event);
+  const color = document.querySelector('input[name="color"]:checked').value;
+  if (await sendPixelData(x, y, color)) drawPixel(x, y, color);
 });
 
-signupBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  formContainer.classList.add("active");
-});
-
-toLogin.addEventListener("click", (e) => {
-  e.preventDefault();
-  formContainer.classList.remove("active");
-});
-
-document.getElementById('loginBtn').addEventListener('click', async (e) => {
-  e.preventDefault();
-  const loginInput = document.querySelector('input[placeholder="Имя пользователя"]');
-  const passwordInput = document.querySelector('input[placeholder="Введите пароль"]');
-
-  const login = loginInput.value;
-  const password = passwordInput.value;
-
-  try {
-    await signIn(login, password);
-    home.classList.remove("show")
-    initAfterLogin(); // Инициализация WebSocket после успешного входа
-  } catch (error) {
-    showError('Ошибка при входе'); // Вывод ошибки
+elements.canvas.addEventListener('mousemove', (event) => {
+  const { x, y } = getPixelCoordinates(event);
+  if (x !== state.prevCoords.x || y !== state.prevCoords.y) {
+    elements.coordinates.textContent = `x: ${x}, y: ${y}`;
+    state.prevCoords = { x, y };
   }
 });
 
-document.getElementById('registerButton').addEventListener('click', async (e) => {
+elements.canvas.addEventListener('mouseup', () => state.isDrawing = false);
+
+// Функция для открытия модального окна
+function openForm() {
+  elements.home.classList.add("show");
+  elements.loginContainer.classList.remove("active");
+}
+
+// Функция для закрытия модального окна
+function closeForm() {
+  elements.home.classList.remove("show");
+  clearFormFields(); // Очищаем поля ввода
+}
+
+function clearFormFields() {
+  const inputs = document.querySelectorAll('.form_container input');
+  inputs.forEach(input => input.value = '');
+}
+
+elements.registerButton.addEventListener("click", async  (e) => {
   e.preventDefault();
 
   const emailInput = document.getElementById('email').value;
@@ -321,64 +217,81 @@ document.getElementById('registerButton').addEventListener('click', async (e) =>
 
   try {
     const result = await signUp(loginInput, emailInput, passwordInput, confirmPasswordInput);
+    console.log(result)
     if (result.status === "success") {
       await  signIn(loginInput,passwordInput)
-      home.classList.remove("show")
     } else {
       showError('Ошибка регистрации.');
     }
+
   } catch (error) {
-    showError('Ошибка при регистрации' );
-  }
+    showError('Ошибка при регистрации');
+  } // Переключаем форму на регистрацию
 });
-function showError(message) {
-  const errorDisplay = document.getElementById('error-display');
-  const errorText = document.getElementById('error-text');
+elements.loginBtn.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const loginInput = document.querySelector('input[placeholder="Имя пользователя"]');
+  const passwordInput = document.querySelector('input[placeholder="Введите пароль"]');
 
-  // Устанавливаем текст ошибки
-  errorText.textContent = message;
-
-  // Показываем ошибку, делая ее видимой
-  errorDisplay.style.opacity = '1';
-
-  // Скрываем ошибку через 5 секунд
-  setTimeout(() => {
-    errorDisplay.style.opacity = '0';
-  }, 5000);
-}
-
-// Функция для инициализации приложения при загрузке страницы
-async function initializeApp() {
-  fetchPixels();
-
-  const authenticated = await isAuthenticated();
-  if (authenticated) {
-      console.log("JWT token найден, инициализация приложения...");
-      initAfterLogin(); // Инициализация приложения, включая WebSocket
-  }
-}
-async function isAuthenticated() {
-
-  const token = window.localStorage.getItem("jwtToken");
-
-
+  const login = loginInput.value;
+  const password = passwordInput.value;
 
   try {
-    const response = await axios.get("http://localhost:8080/auth/validateToken", {
-      headers: {
-        "Authorization": `Bearer ${token}`,
-      },
-    });
-    return response.data.isValid; // Предполагается, что сервер возвращает { isValid: true/false }
+    await signIn(login, password);
+    initAfterLogin(); // Инициализация WebSocket после успешного входа
+    closeForm()
   } catch (error) {
-    showError("Вы не авторизованы")
-    return false; // В случае ошибки также считаем, что пользователь не авторизован
+    alert('Ошибка при входе: ' + error.message); // Вывод ошибки
+  }
+});
+document.addEventListener("click", (event) => {
+  if (!elements.formContainer.contains(event.target) && !elements.formOpenBtn.contains(event.target)) {
+    closeForm();
+  }
+});
+elements.toLoginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  elements.formContainer.classList.remove("active");
+});
+// Обработчик для открытия модального окна при нажатии на кнопку
+elements.formOpenBtn.addEventListener("click", openForm);
+
+// Обработчик для закрытия модального окна при клике вне окна или на кнопку закрытия
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("form_container")) {
+    closeForm();
+  }
+});
+elements.signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  elements.formContainer.classList.add("active");
+});
+
+// Добавляем также обработчик для кнопки закрытия внутри модального окна, если она есть
+document.querySelector(".form-close-btn")?.addEventListener("click", closeForm);
+
+// Проверка авторизации
+async function isAuthenticated() {
+  try {
+    const response = await axios.get("http://localhost:8080/auth/validateToken", {
+      headers: { "Authorization": `Bearer ${auth.token}` },
+    });
+    return response.data.isValid;
+  } catch (error) {
+    showError("Вы не авторизованы");
+    return false;
   }
 }
-// Запускаем инициализацию при загрузке страницы
+
+// Инициализация
+function initializeApp() {
+  fetchPixels();
+  if (auth.token) initAfterLogin();
+}
+
+// Запуск приложения
 window.addEventListener("load", initializeApp);
 
-// Инициализация WebSocket после успешного входа
 async function initAfterLogin() {
-  initWebSocket(); // Подключаемся к WebSocket после авторизации
+  initWebSocket();
 }
